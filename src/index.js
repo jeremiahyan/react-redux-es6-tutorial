9443b882c5ddfdbc0e0e360922ab0f03a2954960
Jeremiah Yan <jeremiah.yxj@gmail.com>
2018-11-30 13:16:44 +0800

    ready to add header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import routes from './routes';
+import './styles/styles.css';  // Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+render(
+  <Router history={browserHistory} routes={routes}/>,
+  document.getElementById('app')
+);
+
