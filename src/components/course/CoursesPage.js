d82d36e30f148b9c8e329729ecf3f98cb8fd756a
Jeremiah Yan <jeremiah.yxj@gmail.com>
2018-12-01 17:16:52 +0800

    mapDispatchToProps

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import connect from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 
 
 class CoursesPage extends React.Component {
@@ -21,13 +22,20 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    // alert(`Saving ${this.state.course.title}`);
+    this.props.createCourse(this.state.course);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
+    // debugger;
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
@@ -45,9 +53,24 @@ class CoursesPage extends React.Component {
 }
 
 
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
+};
+
+
 function mapStateToProps(state, ownProps) {
+  // debugger;
   return {
-    course: state.courses
+    courses: state.courses
+  };
+}
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: (course) => dispatch(courseActions.createCourse(course))
   };
 }
 
